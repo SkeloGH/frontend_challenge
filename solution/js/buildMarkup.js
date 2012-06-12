963204e2fa13818002b2b58df2a6abb7da9c07e3
Skelo <agonzalez@waw.mx>
2012-06-12 14:19:28 -0500

    made it work sideways!

--- solution/js/buildMarkup.js
+++ solution/js/buildMarkup.js
@@ -22,20 +22,43 @@ var galItem = {
 		//fill the gallery
 		this.container.html(textBuffer);
 		this.thumbWidth = this.container.children(':eq(0)').width();
-		console.log(this.thumbWidth);
+		
 
 		this.binding();
 	},
 
 	binding: function(){
 		var that = this;
+		var oldLeft = that.container.css('left');
+
 		this.leftArrow = this.node.find('.leftArrow');
 		this.rightArrow = this.node.find('.rightArrow');
+		
 		$('.leftArrow.active').live('click',function(){
 			$(this).removeClass('active');
-			var newLeft = parseInt(that.container.css('left'))-that.thumbWidth;
+			
+
+			var newLeft = parseInt(that.container.css('left'))-that.thumbWidth-12; // 10px margin and 1px border each side
+			
 			that.container.animate({left:newLeft},function(){
+				that.container.children(':first').appendTo(that.container);
+				that.container.css('left',oldLeft);
 				that.leftArrow.addClass('active');
+			});
+
+			
+
+		});
+
+		$('.rightArrow.active').live('click',function(){
+			$(this).removeClass('active');
+
+			var newRight = parseInt(that.container.css('left'))+that.thumbWidth+12;
+
+			that.container.animate({left:newRight},function(){
+				that.container.children(':last').prependTo(that.container);
+				that.container.css('left',oldLeft);
+				that.rightArrow.addClass('active');
 			});
 		});
 
